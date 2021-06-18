bd78f998599977c819487ad14b8ac48e76c13d32
Ruthi <ruthianet@gmail.com>
2021-06-18 01:52:44 -0300

    terminei

--- terceirizandoFuncoes.js
+++ terceirizandoFuncoes.js
@@ -1,3 +1,17 @@
 // Aula 07 - 18/06/2021
 // Callbacks - Exercício 'Terceirizando funções'
 
+function acaoCarro(Callback) {
+  return Callback();
+}
+
+function andar() {
+  return console.log('O carro está andando.');
+};
+
+function parar() {
+  return console.log('O carro parou.');
+};
+
+acaoCarro(andar);
+acaoCarro(parar);
